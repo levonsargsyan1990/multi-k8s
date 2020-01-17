e20a98a1b56f0458e402e92f59099e1f45d56986
Levon Sargsyan <levonsargsyan1990@gmail.com>
2020-01-17 12:02:07 +0400

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+console.log(keys);
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+sub.subscribe('insert');
+
+function fib(index) {
+    if (index < 1) {
+        return 1;
+    }
+    return fib(index - 1) + fib(index -2);
+}
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+})
